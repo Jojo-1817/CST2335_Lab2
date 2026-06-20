03009446b01d827d730a90d72b4eac489d8d8fee
dereckrocj <54959151+dereckrocj@users.noreply.github.com>
2026-06-19 23:53:49 -0400

    Completed Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textLabel;
+    private EditText editText;
+    private Button buttonPress;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_constraint);
+
+        textLabel = findViewById(R.id.textLabel);
+        editText = findViewById(R.id.editText);
+        buttonPress = findViewById(R.id.buttonPress);
+        checkBox = findViewById(R.id.checkBox);
+
+        buttonPress.setOnClickListener(v -> {
+            textLabel.setText(editText.getText().toString());
+
+            Toast.makeText(
+                    MainActivity.this,
+                    getResources().getString(R.string.toast_message),
+                    Toast.LENGTH_SHORT
+            ).show();
+        });
+
+        checkBox.setOnCheckedChangeListener((CompoundButton cb, boolean b) -> {
+            String status = b ? getString(R.string.on) : getString(R.string.off);
+
+            Snackbar.make(
+                    cb,
+                    getString(R.string.snackbar_message) + " " + status,
+                    Snackbar.LENGTH_LONG
+            ).setAction(
+                    getString(R.string.undo),
+                    click -> cb.setChecked(!b)
+            ).show();
         });
     }
 }
